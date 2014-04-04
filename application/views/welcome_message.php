24a2d1def8b215a7bff5c5f8031cff9a5dad0805
v1ct0r-LH <libertyhackers@cryptolab.net>
2014-04-04 22:58:40 +0000

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,53 +1,59 @@
-     <div class="span9 mainContent" id="homepage">
- 	  <h2>BitWasp</h2>
+        <div class="span9 mainContent" id="homepage">
+
+           <h2>LH - BitWasp SandBox</h2>
 						  
-	  <div class='alert alert-danger'>So long as this message remains in the code, the project is NOT production ready, and should only be used with the Bitcoin Testnet. Let ye be warned.</div>
+	  <div class='alert alert-danger'>CECI EST UNE SANDBOX, touts les transactions réalisées ici sont factice et les payement sont réalisés en utilisant le réseau bitcoin testnet dont la monnaie n'a aucune valeur. Merci de laisser des fonds sur ce site pour permettre à tous de tester.</div>
 			  
-	    <p>Set up your own Bitcoin marketplace, independant of other payment processing services.</p>  
-            <p>Completely avoid the barrier of entry to the Bitcoin Market, and take part in the Bitcoin revolution!</p>  
-            <p>Highly customizable, allowing you to tailor the site's configuration for your users requirements.</p>  
-            <p>The project is open source, and well-reviewed, so rest assured people are working hard to keep your software secure.</p>  
-            <p align='center'><a class="btn btn-success btn-medium" href="https://www.github.com/Bit-Wasp/BitWasp">Check out our source code on GitHub!</a> </p>  
+	    <h1>Bienvenue sur la SandBox BitWasp de Liberty's Hackers <i>French Deep Web</i></h1>
+	      
+            <p>Nous utilisons le logiciel open source <a href ="https://github.com/Bit-Wasp/BitWasp">Bitwasp</a> afin de propulser ce site ainsi que le logiciel <a href="https://www.torproject.org/">Tor</a> afin de preserver notre anonymat et le votre.</p> 
+            <p>l'accès à ce market est complètement gratuit que vous soyez vendeur ou acheteur. Nous retenons seulement 10% des transactions faites à travers notre service d'escrow automatique intégré mais vous pouvez également utiliser uniquement notre système de messagerie sécurisé pour réaliser vos échanges sans aucun frais.</p>
+            <p>Les bénéfices engendrés par l'utilisation de l'escrow servent en priorité à financer le service d’hébergement de Liberty's Hackers, l’excèdent sera reversé aux projets open source que nous soutenons.</p>  
+            
 	    
 	
 	<div class='row-fluid'>
-		<p>Please consider donating to our Bitcoin address, 19EkDTAaGWySZv1QsWxyWwYMZpo7jpvPYe. Donations will go towards hosting, and to provide bounties for bugs.</p>
+		<p>Si ce service vous plait et  que vous désirez nous aider, faite une donation à cette adresse bitcoin : <a href="bitcoin:18axFTskYWpXMTx6kySGjZJHEWFHwd89KA">18axFTskYWpXMTx6kySGjZJHEWFHwd89KA</a>. Les donations iront également au service d'hebergement de Liberty's Hackers et l'exedent ira en priorité au developpeurs de BitWasp.</p>
+    </div>
+</div>
+	</div>
 		 		  
 		    <div class='row-fluid'>
 			  <div class='span4'>
-			    <h3>Fit for all Purposes</h3>
-			    <p>Maybe you want to solely sell your own products on the marketplace, or want to be the next BitMit. BitWasp is built for all purposes, and won't get in the way of what you'd like to do.</p> 
+			    <h3>Achetez et vendez ce que vous voulez</h3>
+			    <p>Nous ne somme soumis à aucun contrôle, par conséquent vous êtes libres de vendre ou d'acheter ce que vous voulez, nous ne nous mélons  pas de vos affaires. Tout ce que vous avez à faire c'est de vous inscrire. Toutefois étant hébergé par Liberty's Hackers et partageant leurs convictions, le racisme et le Child Porn sont strictement interdit.</p> 
           <!--<p><a class="btn btn-success btn-large" href="#">Share your badges now!</a></p>  -->
               </div><!--/span-->  
 			
 			  <div class='span4'>
-			    <h3>Innovative Security</h3>
-			    <p>Upload a PGP key for secure client-side encryption of messages, and to enable features like PGP Two-Factor login to stop attackers dead in their tracks. </p>
+			    <h3>Sécurité Innovante</h3>
+			    <p>Uploadez votre clef PGP pour le chiffrement sécurisé côté client des messages, et pour activer des fonctionnalités telles que l'identification PGP deux facteurs et arrêter net les attaquants.</p>
 			  </div>			
 			  
 			  <div class='span4'>
-			    <h3>Be your own Bank</h3>
-			    <p>Bitcoin eliminate's the need for 3rd party payment processors, so no more per-transaction charges to use an online currency.</p> 
+			    <h3>pas besoin de banque</h3>
+			    <p>Bitcoin élimine le besoin d'un système de payement tripartie, il n'y a donc pas lieu de continuer à engraisser les banques.</p> 
           <!--<p><a class="btn btn-success btn-large" href="#">Share your badges now!</a></p>  -->
               </div><!--/span-->  
 			
 		    </div>	
 		  
-		    <div class='row-fluid'>
+		    <!--<div class='row-fluid'>
 			  <div class='span4'>
-			    <h3>The code is Free! (Gratis & Libre)</h3>
+			    <h3>Le code est libre! (Gratis & Libre)</h3>
 			    <p>as in free speech and free beer! BitWasp is released under the Agorist Licence - it's use is unencumbered by restrictions (and warrantees). The code is yours to deploy in a marketplace, or to release your own, way better version.</p>
-			  </div>				  
+			  </div>-->				  
 				  
 			
 			  <div class='span4'>
-			    <h3>Deal in your local currency</h3>
-			    <p>BitWasp gathers exchange rates for Bitcoin, against USD, GBP, and EUR from some of the leading Price Index API's. Currently comes with built-in support for CoinDesk and BitcoinAverage.com. With this feature, you can set prices in your local currency, and charge users based on the current value of Bitcoin.</p>
+			    <h3>Faites des affaires dans votre monnaie locale</h3>
+			    <p>Le taux d'echange bitcoin est surveillé et mis à jour automatiquement en USD, GBP, et EUR depuis CoinDesk ou BitcoinAverage.com.</p>
 			  </div>
 	
 			  <div class='span4'>
-			    <h3>We're Privacy Conscious</h3>
-			    <p>BitWasp takes the privacy of it's users seriously. All uploaded images are stripped of GPS information, and other meta-data. Private Messages are encrypted using RSA encryption, and completely unreadable until you login and enter your PIN. This presents a barrier to attackers and even nosy administrators.</p>
+			    <h3>Nous respectons votre anonymat</h3>
+			    <p>Nous prenons la vie privée de nos utilisateurs très au sérieux. Toutes les images uploadées sont purgée des informations  GPS et d'autres meta-données. Les messages privés sont chiffrés en utilisant le chiffrement RSA, et complètement illisible à moins que vous ne vous soyez loggé et ayez entré votre PIN. Ce qui représente une barrière contre les attaquants et même les administrateur curieux.</p>
 			  </div>				
 		    </div>			    
         </div>
+
