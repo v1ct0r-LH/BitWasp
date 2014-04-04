3ffd1b41a227e25ce2e70d0634f55a35cfbf0a77
v1ct0r-LH <libertyhackers@cryptolab.net>
2014-04-04 23:01:27 +0000

    add banner
    add

--- application/views/templates/midsection.php
+++ application/views/templates/midsection.php
@@ -1,7 +1,7 @@
-
         </div>
       </div>
     </div>
+    <center> <a href="/"><img src="assets/img/logo.png" alt="LH BitWasp SandBox" /></a><br /></center>
 
     <div class="container-fluid">
       <div class="row-fluid">
